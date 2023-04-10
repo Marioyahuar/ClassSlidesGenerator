958b424437bfc5b308e1e66f043c754eb51772f1
Marioyahuar <marioyahuar@gmail.com>
2023-04-10 16:51:43 -0500

    Manage API Keys with .env files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
 
 // Set up your OpenAI API credentials
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -33,3 +34,15 @@ generateSlideContent(subject)
   .catch((error) => {
     console.error("Error generating slide content:", error);
   });
+
+async function simpleTest() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data);
+}
+
+//simpleTest();
